361ab23bdb760201745f8aab0c844680d91bfd10
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2026-05-11 16:04:56 -0300

    melhora a aleatoriedade dos numeros mockados

--- data.js
+++ data.js
@@ -3,13 +3,13 @@ const resultados = [];
 for (let i = 0; i < 10; i++) {
   resultados.push({
     id: i + 1,
-    interchange: parseFloat((Math.random() * 100).toFixed(2)),
-    taxa_utilizacao_media: parseFloat((Math.random() * 100).toFixed(2)),
-    loss_given_default: parseFloat((Math.random() * 100).toFixed(2)),
-    inadimplencia_fisica_maxima: parseFloat((Math.random() * 100).toFixed(2)),
-    inadimplencia_financeira_maxima: parseFloat((Math.random() * 100).toFixed(2)),
-    limite_maximo_concedivel: parseFloat((Math.random() * 50000).toFixed(2)),
-    multiplicador_maximo_alavancagem: parseFloat((Math.random() * 5).toFixed(2)),
+    interchange: parseFloat((i * 10 + Math.random() * 5).toFixed(2)),
+    taxa_utilizacao_media: parseFloat((i * 10 + Math.random() * 5).toFixed(2)),
+    loss_given_default: parseFloat((i * 10 + Math.random() * 5).toFixed(2)),
+    inadimplencia_fisica_maxima: parseFloat((i * 10 + Math.random() * 5).toFixed(2)),
+    inadimplencia_financeira_maxima: parseFloat((i * 10 + Math.random() * 5).toFixed(2)),
+    limite_maximo_concedivel: parseFloat((i * 5000 + Math.random() * 2000).toFixed(2)),
+    multiplicador_maximo_alavancagem: parseFloat((i * 0.5 + Math.random() * 0.2).toFixed(2)),
     maximo_retorno_obtido: parseFloat((Math.random() * 1000).toFixed(2))
   });
 }
